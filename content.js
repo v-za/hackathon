e941ca3900acdd25b94d8433cc2f19bfcdf8d892
Vinit Patel <vinit@Vinits-MacBook-Pro.local>
2021-03-31 21:52:23 -0400

    pushing

--- content.js
+++ content.js
@@ -1,1 +1,19 @@
-alert("It's Getting real up in here!!!")
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('hopeful')
+    button.addEventListener('click', () => {
+        fetch('https://www.googleapis.com/calendar/v3/users/me/calendarList/c_245h4b65a51pmcbdeqqdh9p9us@group.calendar.google.com')
+            .then(function (response) {
+                return response.json();
+            }
+
+            ).then(function (data) {
+                const trying = data
+                console.log(trying)
+            }
+
+            ).catch(function (err) {
+                console.log("Something Went Wrong", err);
+            })
+    })
+
+})
